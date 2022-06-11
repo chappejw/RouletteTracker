4cef7ebb005a06b30e6bfe4d802440fc6f1e5c68
Jacob Chappelle <jacobchappelle@gmail.com>
2022-06-10 23:49:03 -0700

    Initial Commit

---
+++ RouletteTracker/RouletteTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteTrackerApp.swift
+//  RouletteTracker
+//
+//  Created by Jacob Chappelle on 2022-06-10.
+//
+
+import SwiftUI
+
+@main
+struct RouletteTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
